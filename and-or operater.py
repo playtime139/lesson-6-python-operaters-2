4383f4f03ff0243b21124d04ba450d2d99d81fc4
playtime139 <ryankimungui@gmail.com>
2025-10-25 14:50:09 +1100

    an-opr operaters.py

---
+++ and-or operater.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast one number has boolean value as false")
+
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+    print("Either of the numbers is greater than zero")
+else:
+    print("none of the numbers is greater than zero")
+
+    if b > 0 or c > 0:
+        print("either of the numbers is greater than zero")
+    else:
+        print("none of the numbers greater than zero")
